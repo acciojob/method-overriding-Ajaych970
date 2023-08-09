e191060dec29edeeb430f50fc2975a71d766f1f6
Mrfantasticc <ajay.electromania@gmail.com>
2023-08-09 12:06:11 +0530

    Attempt 1

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,10 @@
 package com.driver;
 
 public class Main {
+    public static void main(String[] args) {
+        A a = new A();
+        B b = new B();
+        System.out.println(b.meth());
+    }
   
 }
